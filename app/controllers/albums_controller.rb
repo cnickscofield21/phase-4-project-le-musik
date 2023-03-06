9a0d96df4139431651f8bc4659fb9e90dcf23cb9
Josiah Storrie <jpstorrie@gmail.com>
2023-03-06 14:57:46 -0700

    cleaned up scaffolded controllers

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -8,53 +8,25 @@ class AlbumsController < ApplicationController
 
   # GET /albums/1 or /albums/1.json
   def show
-  end
-
-  # GET /albums/new
-  def new
-    @album = Album.new
-  end
-
-  # GET /albums/1/edit
-  def edit
+    @album = Album.find(params[:id])
+    render json: @album, status: :ok
   end
 
   # POST /albums or /albums.json
   def create
-    @album = Album.new(album_params)
-
-    respond_to do |format|
-      if @album.save
-        format.html { redirect_to album_url(@album), notice: "Album was successfully created." }
-        format.json { render :show, status: :created, location: @album }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @album.errors, status: :unprocessable_entity }
-      end
-    end
+    @album = Album.create!(album_params)
+    render json: @album, status: :created
   end
 
   # PATCH/PUT /albums/1 or /albums/1.json
   def update
-    respond_to do |format|
-      if @album.update(album_params)
-        format.html { redirect_to album_url(@album), notice: "Album was successfully updated." }
-        format.json { render :show, status: :ok, location: @album }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @album.errors, status: :unprocessable_entity }
-      end
-    end
+      @album.update!(album_params)
+      render json: @album, status: :updated
   end
 
   # DELETE /albums/1 or /albums/1.json
   def destroy
     @album.destroy
-
-    respond_to do |format|
-      format.html { redirect_to albums_url, notice: "Album was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
@@ -65,6 +37,6 @@ class AlbumsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def album_params
-      params.require(:album).permit(:title, :year, :image)
+      params.permit(:title, :year, :image)
     end
 end
--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -4,57 +4,30 @@ class TracksController < ApplicationController
   # GET /tracks or /tracks.json
   def index
     @tracks = Track.all
+    render json: @tracks, status: :ok
   end
 
   # GET /tracks/1 or /tracks/1.json
   def show
-  end
-
-  # GET /tracks/new
-  def new
-    @track = Track.new
-  end
-
-  # GET /tracks/1/edit
-  def edit
+    @track = Track.find(params[:id])
+    render json: @track, status: :ok
   end
 
   # POST /tracks or /tracks.json
   def create
-    @track = Track.new(track_params)
-
-    respond_to do |format|
-      if @track.save
-        format.html { redirect_to track_url(@track), notice: "Track was successfully created." }
-        format.json { render :show, status: :created, location: @track }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @track.errors, status: :unprocessable_entity }
-      end
-    end
+    @track = Track.create!(track_params)
+    render json: @track, status: :created
   end
 
   # PATCH/PUT /tracks/1 or /tracks/1.json
   def update
-    respond_to do |format|
-      if @track.update(track_params)
-        format.html { redirect_to track_url(@track), notice: "Track was successfully updated." }
-        format.json { render :show, status: :ok, location: @track }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @track.errors, status: :unprocessable_entity }
-      end
-    end
+  @track.update!(track_params)
+  render json: @track, status: :updated
   end
 
   # DELETE /tracks/1 or /tracks/1.json
   def destroy
     @track.destroy
-
-    respond_to do |format|
-      format.html { redirect_to tracks_url, notice: "Track was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
@@ -65,6 +38,6 @@ class TracksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def track_params
-      params.require(:track).permit(:track, :title, :duration, :year, :album_id, :artist_id)
+      params.permit(:track, :title, :duration, :year, :album_id, :artist_id)
     end
 end
