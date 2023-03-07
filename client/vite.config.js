4146bb769b5f76c6ee8387a872c19b43509c1e2e
Josiah Storrie <jpstorrie@gmail.com>
2023-03-07 10:42:22 -0700

    added proxy(untested)

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3001, // changed my outgoing port to 3000 instead of the default 5173
+    proxy: {
+      "/api": {
+        // changed my proxy path to /api
+        target: "http://127.0.0.1:3000/", // whenever we type /api in our fetch it will be recognized as this route
+        changeOrigin: true,
+        secure: false, // handle security issues
+        rewrite: (path) => path.replace(/^\/api/, ""), // rewrite the path to remove the /api
+      },
+    },
+  },
   plugins: [react()],
-})
+});
