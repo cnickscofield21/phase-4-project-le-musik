69a7c1bcfb9df3942669c4ef253ef074940a3739
Josiah Storrie <jpstorrie@gmail.com>
2023-03-07 15:23:38 -0700

    added rack-cors and tested /login

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,4 +1,19 @@
 function Home(){
+    const user = {
+        username: "nick",
+        password: "password"
+      }
+
+    const loginObj = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(user)
+    }
+
+    fetch('http://localhost:3000/login', loginObj)
+    .then(r => r.json())
+    .then(console.log)
+
     return (<>
     <h1>Homepage with albums</h1>
     </>)
